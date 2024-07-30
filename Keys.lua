404cfe4cf02a92c916a4f81a049b0143adffc5f3
Virxq <kanapeczek0@gmail.com>
2024-07-30 23:35:19 +0200

    Create Keys.lua

---
+++ Keys.lua
@@ -0,0 +1,5 @@
+return {
+    ["exampleKey1"] = os.time() + (10 * 24 * 60 * 60),  -- 10 dni od teraz
+    ["exampleKey2"] = os.time() + (5 * 24 * 60 * 60),   -- 5 dni od teraz
+    -- Dodaj tutaj inne klucze
+}
